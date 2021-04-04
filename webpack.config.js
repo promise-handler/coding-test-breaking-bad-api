eb6dca0d0ff6c807fc58b9e051ea35d02c3c316d
SzymonKrysztofiak <s.krysztofiak@outlook.com>
2021-04-04 09:02:24 +0200

    Add simple webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    contentBase: './dist',
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+}
