47c8401cdff014123a6468679fdf203470912728
SzymonKrysztofiak <s.krysztofiak@outlook.com>
2021-04-04 08:27:08 +0200

    Handle form

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-// TODO: Handle form
+let formValue = "";
+const form = document.querySelector('.BreakingBad__form');
+
+const handleForm = (event) => {
+  event.preventDefault();
+  const input = form.elements['search-input'];
+
+  formValue = input.value;
+}
+
+form.addEventListener('submit', handleForm);
+
 // TODO: Transform data depending on the type (String, Array)
 // TODO: Handle API calls with a service method
 // TODO: Transform data using the search phrase
